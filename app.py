e96135426f2c63da38071423eed9861d5866ad19
Murat Simsek, Ph.D <muratsimsek003@gmail.com>
2022-09-01 21:36:56 +0300

    commit

--- app.py
+++ app.py
@@ -1,32 +1,32 @@
+import json
 import pickle
+
 from flask import Flask,request,app,jsonify,url_for,render_template
 import numpy as np
 import pandas as pd
-import os
 
-port = int(os.environ.get('PORT', 5000))
 app=Flask(__name__)
-
-##Load the model (Model yükleme)
-model=pickle.load(open("completeMLRegressionProject/regmodel.pkl","rb"))
-
-@app.route("/")
-
+## Load the model
+regmodel=pickle.load(open('regmodel.pkl','rb'))
+@app.route('/')
 def home():
-    return render_template("home.html")
+    return render_template('home.html')
 
-
+@app.route('/predict_api',methods=['POST'])
+def predict_api():
+    data=request.json['data']
+    print(data)
+    output=regmodel.predict(data)
+    print(output[0])
+    return jsonify(output[0])
 
 @app.route('/predict',methods=['POST'])
 def predict():
     data=[float(x) for x in request.form.values()]
-    print(data)
-    data=[np.array(data)]
-    output=model.predict(data)
+    output=regmodel.predict(data)[0]
     return render_template("home.html",prediction_text="Boston City House Price Prediction is {}".format(output))
 
 
 
 if __name__=="__main__":
-    #app.run(debug=True)
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(debug=True)
